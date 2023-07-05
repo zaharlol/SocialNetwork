e2b84ee3cbdd19762bd8d56334e416c98d9d3c31
zaharlol <kovtun.zahar12@mail.ru>
2023-07-05 12:28:43 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/BLL/Models/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string SenderEmail { get; set; }
+        public string RecipientEmail { get; set; }
+
+        public Message
+            (
+            int id,
+            string content,
+            string senderEmail,
+            string recipientEmail
+            )  
+        { 
+            Id = id;
+            Content = content;
+            SenderEmail = senderEmail;
+            RecipientEmail = recipientEmail;
+        }
+    }
+}
---
+++ SocialNetwork/BLL/Services/MessageService.cs
@@ -0,0 +1,71 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class MessageService
+    {
+        IUserRepository userRepository;
+        IMessageRepository messageRepository;
+        public MessageService()
+        {
+            userRepository = new UserRepository();
+            messageRepository = new MessageRepository();
+        }
+        public IEnumerable<Message> GetIncomingMessagesByUserId(int recipientId)
+        {
+            var messages = new List<Message>();
+
+            messageRepository.FindByRecipientId(recipientId).ToList().ForEach(m =>
+            {
+                var senderUserEntity = userRepository.FindById(m.sender_id);
+                var recipientUserEntity = userRepository.FindById(m.recipient_id);
+
+                messages.Add(new Message(m.id, m.content, senderUserEntity.email, recipientUserEntity.email));
+            });
+
+            return messages;
+        }
+
+        public IEnumerable<Message> GetOutcomingMessagesByUserId(int senderId)
+        {
+            var messages = new List<Message>();
+
+            messageRepository.FindBySenderId(senderId).ToList().ForEach(m =>
+            {
+                var senderUserEntity = userRepository.FindById(m.sender_id);
+                var recipientUserEntity = userRepository.FindById(m.recipient_id);
+
+                messages.Add(new Message(m.id, m.content, senderUserEntity.email, recipientUserEntity.email));
+            });
+
+            return messages;
+        }
+        public void Send(MessageSending messageSending)
+        {
+            if (String.IsNullOrEmpty(messageSending.Content)) throw new ArgumentNullException();
+
+            if (messageSending.Content.Length > 5000) throw new ArgumentOutOfRangeException();
+
+            var findUser = this.userRepository.FindByEmail(messageSending.RecipientEmail);
+            if (findUser is null) throw new UserNotFoundException();
+
+            var message = new MessageEntity
+            {
+                content = messageSending.Content,
+                sender_id = messageSending.SenderId,
+                recipient_id = findUser.id
+            };
+
+            if (this.messageRepository.Create(message) == 0) 
+                throw new Exception();
+            
+        }
+    }
+}
